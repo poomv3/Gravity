3adef6af26a341495b827bcec94c4a7484fb95c8
poomv3 <ericdraganoiu@gmail.com>
2025-06-26 21:47:30 +0100

    orbit

--- Gravity/Program.cs
+++ Gravity/Program.cs
@@ -5,14 +5,15 @@ using Gravity;
 
 // Simple gravity (no air resistance, no friction)
 
-Vector2 velocity = new Vector2(5f, 0f);
+Vector2 objectVelocity = new Vector2(5f, -5f);
+Vector2 planetVelocity = new Vector2(0f, 5f);
 
 //center of grid (object)
-Vector2 objectPosition = new Vector2(70f,25f);
-Vector2 planetPosition = new Vector2(75f,15f);
-float planetMass = 100000000f;
-float objectMass = 100000f;
-float G = 0.000000000066743f;
+Vector2 objectPosition = new Vector2(30f,15f);
+Vector2 planetPosition = new Vector2(50f,5f);
+float planetMass = 150f;
+float objectMass = 10f;
+float G = 1f;
 
 
 string[,] grid = new string[35, 150];
@@ -23,19 +24,21 @@ for (int x = 0; x < grid.GetLength(0); x++)
     {
         if (x == 15 && y == 75)
         {
-            grid[x, y] = "X";
+            grid[x, y] = "█";
         }
         grid[x, y] = " ";
     }
 }
 
-float deltaTime = 0.1f;
+float deltaTime = 0.5f;
 int steps = 1000;
 float time = 0f;
 
 
 for (int i = 0; i < steps; i++)
 {
+    
+    
     Vector2 direction = planetPosition - objectPosition;
     float distance = direction.Length();
     Vector2 unitDirection = Vector2.Normalize(direction);
@@ -44,23 +47,36 @@ for (int i = 0; i < steps; i++)
 
     Vector2 gravity = gravitationalForce * unitDirection;
     
-    velocity += gravity * deltaTime;
-    objectPosition += velocity * deltaTime;
+    objectVelocity += gravity * deltaTime;
+    objectPosition += objectVelocity * deltaTime;
     
-    int posX = (int)Math.Round(objectPosition.X);
-    int posY = (int)Math.Round(objectPosition.Y);
+    planetVelocity -= gravity * deltaTime;
+    planetPosition += planetVelocity * deltaTime;
     
+    int objectposX = (int)Math.Round(objectPosition.X);
+    int objectposY = (int)Math.Round(objectPosition.Y);
+    
+    int planetposX = (int)Math.Round(planetPosition.X);
+    int planetposY = (int)Math.Round(planetPosition.Y);
+    
+    Console.WriteLine(objectposX);
+    Console.WriteLine(objectposY);
+    
+    Console.WriteLine(planetposX);
+    Console.WriteLine(planetposY);
+    
+
     // output the grid
     for (int x = 0; x < grid.GetLength(0); x++)
     {
         Console.WriteLine();
         for (int y = 0; y < grid.GetLength(1); y++)
         {
-            if (30 - x == posY && y == posX)
+            if (30 - x == objectposY && y == objectposX)
             {
-                Console.Write(grid[x, y] = "O");
+                Console.Write(grid[x, y] = "█");
             }
-            else if (30 - x == 15 && y == 75)
+            else if (30 - x == planetposY && y == planetposX)
             {
                 Console.Write(grid[x, y] = "X");
             }
@@ -71,8 +87,9 @@ for (int i = 0; i < steps; i++)
         }
     }
     time += deltaTime;
-    Thread.Sleep(100);
+    Thread.Sleep(500);
     Console.Clear();
+
         
         // Console.WriteLine($"Position of object: {position}, Time: {Math.Round(time,2)}s");
 
